88d7ba1cf629671d9411f617a8e15a5cd165618e
David Yu <lewen.yu@gmail.com>
2012-11-25 01:28:52 -0500

    flashing screen!

--- src/com/cpsc434/stroboscopik/StrobeActivity.java
+++ src/com/cpsc434/stroboscopik/StrobeActivity.java
@@ -1,25 +1,16 @@
 package com.cpsc434.stroboscopik;
 
-import java.io.File;
-
-
-import com.cpsc434.stroboscopik.util.SystemUiHider;
-import edu.emory.mathcs.jtransforms.fft.DoubleFFT_1D;
-
 import android.annotation.TargetApi;
 import android.app.Activity;
-import android.content.Context;
-import android.media.AudioFormat;
-import android.media.AudioRecord;
-import android.media.MediaRecorder;
-import android.os.AsyncTask;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
 import android.os.Handler;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+
+import com.cpsc434.stroboscopik.util.SystemUiHider;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -55,17 +46,9 @@ public class StrobeActivity extends Activity {
 	 * The instance of the {@link SystemUiHider} for this activity.
 	 */
 	
-    protected InitTask _initTask;
-    
 	private SystemUiHider mSystemUiHider;
-	public AudioRecord audioRecord; 
-    public int mSamplesRead; //how many samples read 
-    public int recordingState;
-    public int buffersizebytes; 
-    public int channelConfiguration = AudioFormat.CHANNEL_IN_MONO; 
-    public int audioEncoding = AudioFormat.ENCODING_PCM_16BIT; 
-    public static short[] buffer; //+-32767 
-    public static final int SAMPPERSEC = 44100; //samp per sec 8000, 11025, 22050 44100 or 48000
+    private Handler mHandler = new Handler(); //for dummy flashing
+    public long flashTimeStamp = 0;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -76,6 +59,7 @@ public class StrobeActivity extends Activity {
 		final View controlsView = findViewById(R.id.fullscreen_content_controls);
 		final View contentView = findViewById(R.id.fullscreen_content);
 
+		startFlashing();
 		// Set up an instance of SystemUiHider to control the system UI for
 		// this activity.
 		mSystemUiHider = SystemUiHider.getInstance(this, contentView,
@@ -125,7 +109,6 @@ public class StrobeActivity extends Activity {
 		contentView.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
-				startDummyRecording();
 				if (TOGGLE_ON_CLICK) {
 					mSystemUiHider.toggle();
 				} else {
@@ -141,114 +124,35 @@ public class StrobeActivity extends Activity {
 				mDelayHideTouchListener);
 	}
 	
-	private void startDummyRecording() {
-        //buffersizebytes = AudioRecord.getMinBufferSize(SAMPPERSEC,channelConfiguration,audioEncoding); //4096 on ion 
-        //buffer = new short[buffersizebytes]; 
-        //audioRecord = new AudioRecord(android.media.MediaRecorder.AudioSource.MIC,SAMPPERSEC,channelConfiguration,audioEncoding,buffersizebytes); //constructor 
-
-        //_initTask = new InitTask();
-        //_initTask.execute( this );
-       
-        try {
-	        WavFile dummy = WavFile.openWavFile( new File( Environment.getExternalStorageDirectory().getAbsolutePath() + "/SHM-DontYouWorryChild.wav" ) );
-	        
-	        int numChannels = dummy.getNumChannels();
-	        
-	        double[] buffer = new double[100 * numChannels];
-	        
-	        int framesRead;
-	        double min = Double.MAX_VALUE;
-	        double max = Double.MIN_VALUE;
-	        
-	        do
-	         {
-	            // Read frames into buffer
-	            framesRead = dummy.readFrames(buffer, 100);
-
-	            // Loop through frames and look for minimum and maximum value
-	            for (int s=0 ; s<framesRead * numChannels ; s++)
-	            {
-	               if (buffer[s] > max) max = buffer[s];
-	               if (buffer[s] < min) min = buffer[s];
-	            }
-	         }
-	         while (framesRead != 0);
-	        
-	         DoubleFFT_1D fft = new DoubleFFT_1D(1024); // 1024 is size of array
-	         fft.realForward( buffer );
-
-	         // Close the wavFile
-	         dummy.close();
-	         
-	        
-        } catch ( Exception e ) {
-        	e.printStackTrace();
-        }
-    }
+	private void startFlashing() {
+        mHandler.postDelayed(mFlashTask, 1000);
+	}
 	
-    protected class InitTask extends AsyncTask<Context, Integer, String>
-    {
-        // -- run intensive processes here
-        // -- notice that the datatype of the first param in the class definition matches the param passed to this method 
-        // -- and that the datatype of the last param in the class definition matches the return type of this mehtod
-                @Override
-                protected String doInBackground( Context... params ) 
-                {
-                        //-- on every iteration
-                        //-- runs a while loop that causes the thread to sleep for 50 milliseconds 
-                        //-- publishes the progress - calls the onProgressUpdate handler defined below
-                        //-- and increments the counter variable i by one
-                        //int i = 0;
-
-                    audioRecord.startRecording();
-
-                        while( true ) 
-                        {
-                                try{
-                                        mSamplesRead = audioRecord.read(buffer, 0, buffersizebytes);
-
-                                        int amp;
-
-                                        for(int i = 0; i < buffersizebytes - 1; i++){
-                                            amp = (int)buffer[i];
-                                            publishProgress( amp );
-                                        }
-
-                                } catch( Exception e ){                        
-                                }
-                        }
-                }
-
-                // -- gets called just before thread begins
-                @Override
-                protected void onPreExecute() 
-                {
-                        //Log.i( "makemachine", "onPreExecute()" );
-                        super.onPreExecute();
-
-                }
-
-                // -- called from the publish progress 
-                // -- notice that the datatype of the second param gets passed to this method
-                @Override
-                protected void onProgressUpdate(Integer... values) 
-                {
-                        super.onProgressUpdate(values);
-                        //Log.i( "makemachine", "onProgressUpdate(): " +  String.valueOf( values[0] ) );
-                        Log.d( "audioInput", String.valueOf(values[0]) );
-                }
-
-                // -- called as soon as doInBackground method completes
-                // -- notice that the third param gets passed to this method
-                @Override
-                protected void onPostExecute( String result ) 
-                {
-                        super.onPostExecute(result);
-                        //Log.i( "makemachine", "onPostExecute(): " + result );
-                }   
-
-
-     }
+	private Runnable mFlashTask = new Runnable() {
+        public void run() {
+        	final View contentView = findViewById(R.id.fullscreen_content);
+        	contentView.setBackgroundColor(Color.argb(255, 255, 255, 255));
+        	flashTimeStamp = System.currentTimeMillis();
+        
+        	mHandler.postDelayed(mFadeTask, 10); //ideally these are constants defined elsewhere
+            mHandler.postDelayed(this, 500);
+        }
+    };
+    
+    private Runnable mFadeTask = new Runnable() {
+        public void run() {
+        	final View contentView = findViewById(R.id.fullscreen_content);
+			long elapsed = System.currentTimeMillis() - flashTimeStamp;
+			int c = 255 - (int) ((double) elapsed/200.0 * 255);
+			c = c < 0 ? 0 : c; //normalize c
+        	contentView.setBackgroundColor(Color.argb(255, c, c, c));
+        	
+        	if (c < 255) {
+	            mHandler.postDelayed(this, 5);
+        	}
+            
+        }
+    };
 
 	@Override
 	protected void onPostCreate(Bundle savedInstanceState) {
